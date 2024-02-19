34568f0dda0f1fc8552d2f9958ec1bc62b76e229
Etienne Vautherin <etienne@vautherin.com>
2024-02-19 16:54:45 +0100

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Etienne Vautherin on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
