f86abaeb39f40160c53ec66d2cb0732ddcbc59d3
Etienne Vautherin <etienne@vautherin.com>
2024-02-19 17:21:36 +0100

    NameField

--- Hello/ContentView.swift
+++ Hello/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    @State private var name = ""
+    
     var body: some View {
+        let upperName = name.uppercased()
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Counter: \(counter) \(name)")
+            Button("Increment") {
+                counter += 1
+            }
+//            TextField("Name", text: $name)
+            NameField(name: $name)
         }
         .padding()
     }
 }
+
+struct NameField: View {
+//    @State var rawName = ""
+    @Binding var name: String
+    
+//    init(rawName: String = "", name: Binding<String>) {
+//        self.rawName = rawName
+//        self._name = name
+//    }
+    
+    var body: some View {
+        TextField("Name", text: $name)
+//            .onChange(of: rawName) { oldValue, newValue in
+//                name = newValue.uppercased()
+//            }
+    }
+}
+
+
 
 #Preview {
     ContentView()
