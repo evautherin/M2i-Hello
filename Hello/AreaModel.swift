2433024e5bc79f3ffadbabdc1d5294194f82b695
Etienne Vautherin <etienne@vautherin.com>
2024-02-20 10:37:24 +0100

    AreaModel

---
+++ Hello/AreaModel.swift
@@ -0,0 +1,17 @@
+//
+//  AreaModel.swift
+//  Hello
+//
+//  Created by Etienne Vautherin on 20/02/2024.
+//
+
+import SwiftUI
+
+@Observable
+class AreaModel {
+    var width = 50.0
+    var height = 50.0
+    
+    var area: Double { width*height }
+    var areaString: String { String(format: "Angle: %.2f", area) }
+}
